cad9cfe4aec70782964c8f094adf5a1b2def0d69
emtiazahmed27 <emtihstu127@gmail.com>
2022-07-17 15:10:19 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  //states
+  const [todoTitle,setTodoTitle]=useState("");
+  const [todoList, setTodoList]=useState([]);
+  const [isEditable, setEditable]=useState(false);
+  const [editableTodo, setEditableTodo]=useState(null);
+
+
+  const createHandler= (event)=>{
+    event.preventDefault();
+
+    const newTodo={
+      id:Date.now(),
+      title: todoTitle
+    }
+
+    setTodoList([newTodo
+      , ...todoList])
+    setTodoTitle("");
+  }
+
+  const editHandler= (id) => {
+    const toBeEdited= todoList.find( todo => todo.id === id)
+    console.log(toBeEdited);
+    console.log(isEditable);
+    setEditable(true);
+    setEditableTodo(toBeEdited);
+    setTodoTitle(toBeEdited.title);
+  }
+  const updateHandler= (event) =>{
+    event.preventDefault()
+    editableTodo.title= todoTitle;
+    setTodoTitle("")
+    setEditable(false);
+    setEditableTodo(null);
+  }
+
+  const deleteHandler= (id) =>{
+    //j id ta milbe na tader filter kore newTodoList banabe
+    const newTodoList = todoList.filter( todo => todo.id != id);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO with React CRUD</h1>
+      <form action="">
+        <input type="text" name="todoTitle" id="input"
+        value={todoTitle} placeholder='input a valid TODO' onChange={ (event) => setTodoTitle(event.target.value)} />
+
+        <button className='addBtn blueBtn' onClick={(event) => isEditable === true ? updateHandler(event) : createHandler(event)}>{ isEditable === true ? "Update button": "Add button" }</button>
+
+      </form>
+      <ul>
+        {todoList.map( todo => (
+          <li>
+            <p>{todo.title}</p>
+
+            {/* edit button fnctionality */}
+            <button className='editBtn pinkBtn' onClick={ () => editHandler(todo.id) }>Edit</button>
+
+            {/* delete button functionality */}
+            <button className='delBtn redButn'onClick={ () =>{
+            //  deleteHandeler function k id ditesi
+             deleteHandler(todo.id)
+              console.log(todo.id);
+            }}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
